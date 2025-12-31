beb6277af0151a61a00d83f162ac9a3e5d6e4f1d
Rosemoe <2073412493@qq.com>
2025-12-31 14:40:42 +0800

    feat: add text for chart name and renderer information

--- example.py
+++ example.py
@@ -4,6 +4,6 @@ import skia as sk
 
 if __name__ == '__main__':
     chart = parse(open(f'assets/gengaozo.json', 'r').read())
-    renderer = ChainbeetRenderer(chart, ChainbeetRenderConfig())
+    renderer = ChainbeetRenderer(chart, chart_name='G e n g a o z o [EXTRA 10]')
     image = renderer.render()
     image.save(open('gengaozo.png', 'wb'), sk.EncodedImageFormat.kPNG)
